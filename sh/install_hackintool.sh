808963ced91892d20d50a103d74e6cf6ba7709dd
gendseo <aubo@gendseo.top>
2019-06-28 16:18:26 +0800

    add install_hackintool.sh

---
+++ sh/install_hackintool.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# description: dropbox install with proxy
+# author     : gendseo
+# create     : 28/06/2019
+# update     : 28/06/2019
+
+wget -c -t 0 -P ~/Downloads/dev http://headsoft.com.au/download/mac/Hackintool.zip
+unzip -q -o ~/Downloads/dev/Hackintool.zip -d ~/Downloads/dev
+mv ~/Downloads/Hackintool.app /Applications/Hackintool.app
